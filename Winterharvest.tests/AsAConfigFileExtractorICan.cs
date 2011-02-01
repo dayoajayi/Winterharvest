1e5126e2401b8d511b7033e9fd568f6f5df69adc
Jim Sowers <jpsowers@yahoo.com>
2011-01-31 19:59:39 -0600

    first failing test added

--- Winterharvest.tests/AsAConfigFileExtractorICan.cs
+++ Winterharvest.tests/AsAConfigFileExtractorICan.cs
@@ -20,5 +20,10 @@ namespace Winterharvest.tests
             Assert.IsNotNull(_configFileExtractor);
         }
 
+        [Test]
+        public void AndReturnAKeyAndValueFromAConfigFile()
+        {
+            Assert.AreEqual("stinky", _configFileExtractor.Extract(new ExtractionPoint()));
+        }
     }
 }
