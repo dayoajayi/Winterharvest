d0a0741deb5a0d0c28e9abc655bdd806f4459a63
Jim Sowers <jpsowers@yahoo.com>
2011-02-06 22:33:55 -0600

    Got test to run but the method we are trying to use may be bunk

--- harvest/ConfigFileExtractor.cs
+++ harvest/ConfigFileExtractor.cs
@@ -1,22 +1,37 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 
 namespace harvest
 {
     public class ConfigFileExtractor : IExtractor
     {
+        public ConfigFileExtractor()
+        {
+            string n = "Nope";
+        }
+
         public IEnumerable<MetaDataElement> Extract(ExtractionPoint extractionPoint)
         {
-            var o = ConfigurationManager.OpenExeConfiguration(@".\test.config");
-            string[] blah = o.AppSettings.Settings.AllKeys;
+            List<MetaDataElement> results = new List<MetaDataElement>();
 
-            foreach (var key in blah)
+            string appPathAndName = Path.Combine(@"C:\Development\WinterHarvest\Winterharvest.tests", "test.exe");
+            string appPath = Environment.CurrentDirectory;
+
+            var configuration = ConfigurationManager.OpenExeConfiguration(appPathAndName);
+           
+            string[] keyCollection = configuration.AppSettings.Settings.AllKeys;
+
+            foreach (var key in keyCollection)
             {
-                var val = o.AppSettings.Settings[key];
+                var val = configuration.AppSettings.Settings[key];
 
-                yield return new ConfigurationMetaDataElement(){Key = key, Value = val.Value};
+                //yield return new ConfigurationMetaDataElement() {Key = key, Value = val.Value};
+                results.Add(new ConfigurationMetaDataElement() {Key = key, Value = val.Value});
             }
-            
+            return results;
         }
 
         public string GetKey()
