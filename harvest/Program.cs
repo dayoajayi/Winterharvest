1102f7ef43ae4306a84430b995ece6ccf8c3cc75
Dayo Ajayi <dayo.ajayi@gmail.com>
2011-02-22 19:57:25 -0600

    roughing in Options.cs

--- harvest/Program.cs
+++ harvest/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Mono.Documentation;
 
 namespace harvest
 {
@@ -9,6 +8,13 @@ namespace harvest
     {
         static void Main(string[] args)
         {
+            var o = new Options();
+            o.Add("d", s => {
+                               Console.WriteLine("bob");
+                           });
+           
+            IEnumerable<string> a=  o.Parse(new string[] {"/d"});
+            var b = o.Parse(new[] {"a"});
         }
     }
 }
